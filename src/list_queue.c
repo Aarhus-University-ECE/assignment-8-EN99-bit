60244dcede73f397b2439720bd9c0d831a5ae154
EN99-bit <112312714+EN99-bit@users.noreply.github.com>
2022-11-09 10:15:43 +0100

    Last index wont sort & Dequeue missing

--- src/list_queue.c
+++ src/list_queue.c
@@ -1,29 +1,53 @@
 #include "list_queue.h"
 
-
-void init_queue(queue *q)
+void init_queue(queue *q) // if front and rear points are both NULL, it means
 {
-q->front = NULL;
-q->size = 0;
-q->rear = NULL;
+  q->size = 0;
+  q->front = NULL;
+  q->rear = NULL;
 }
 
 int empty(queue *q)
 {
-if (q->size = NULL){
-  return 1; //return true if size is NULL/empty
-}
-else{
-  return 0; //return false if size
-}
+  if (q->size == 0)
+  {
+    return 1; // return true if size is NULL/empty
+  }
+  else
+  {
+    return 0; // return false if size
+  }
 }
 
-void enqueue(queue *q, int x)
+void enqueue(queue *q, int x) // Function to add an element to the queue
 {
   // Add your enqueue function
+  qnode *element = malloc(sizeof(qnode)); // Adding a node to the end of the queue
+
+  element->data = x;    // with x as data and NULL as last in next*
+  element->next = NULL; // last element in queue always points at NULL
+
+  if (q->rear == 0)
+  {
+    q->rear = element;
+    q->front = element;
+    q->rear->next = NULL;
+  }
+  else
+  {
+    q->rear->next = element;
+    q->rear = element;
+  }
+
+  // 1: Build new node with data
+  // 2: Check if the queue is empty
+  // 3: If queue is empty then create node at front and rear
+  // 4: Else make next of rear as new node and rear
+  // 5: Add one to size of queue
+  q->size++;
 }
 
-int dequeue(queue *q)
+int dequeue(queue *q)// Add your dequeue function
 {
-  // Add your dequeue function
+  
 }
